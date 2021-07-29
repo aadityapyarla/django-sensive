f56382bc55ae6741c35a014791d011937807b356
Aaditya Pyarla <aadityapyarla82@gmail.com>
2021-07-29 10:41:54 +0530

    Add registeration using custom user model
    - Added registeration to the db
    - Integrated email verfication using auth tokens
    - Added token verification
    - Optimized Error handling

--- accounts/render_views.py
+++ accounts/render_views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from .models import Account
+from django.contrib import messages
+from . views import register, send_auth_token
+from django.contrib.auth import logout
 
 
 def renderLogin(request):
@@ -6,6 +10,8 @@ def renderLogin(request):
 
 
 def renderRegister(request):
+    if request.method == 'POST':
+        return register(request)
     return render(request, 'accounts/register.html')
 
 
@@ -15,3 +21,28 @@ def renderProfile(request):
 
 def renderNewPost(request):
     return render(request, 'accounts/new_post.html')
+
+
+def renderAuthTokenSent(request):
+    if request.method == 'POST':
+        email = request.session['resend_email']
+        auth_token = request.session['auth_token']
+        send_auth_token(request, auth_token, email)
+
+    return render(request, 'accounts/auth_token_sent.html')
+
+
+def verifyAuthToken(request, auth_token):
+    account = Account.objects.filter(auth_token=auth_token).first()
+    if account.is_active:
+        if account.is_verified:
+            messages.info(request, "Your email was already verified")
+            return redirect('home')
+        else:
+            account.is_verified = True
+            account.save()
+            messages.info(request, "Your email was successfully verified!")
+            return redirect('home')
+    else:
+        messages.error(request, "Your account is disabled")
+        return redirect('home')

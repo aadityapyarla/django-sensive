7204cec8264bc4131a13c144b22f7bfa4a39a3c3
Aaditya Pyarla <aadityapyarla82@gmail.com>
2021-07-29 11:14:47 +0530

    Add login feature
    - Added login system
    - Checks if user is verified and active during login
    - Identifies the user based on thier login
    - Optimized error handling

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('register/', render_views.renderRegister, name="register"),
     path('profile/', render_views.renderProfile, name="profile"),
     path('new_post/', render_views.renderNewPost, name="new_post"),
+
+    # ! Data Processing URL Routes
+    # ? These URL's don't render, instead they process the given data and redirects the user
+    path('logout/', render_views.renderLogout, name="logout"),
+    path('auth_token_sent/', render_views.renderAuthTokenSent, name="auth_token_sent"),
+    path('auth_token_verify/<auth_token>/', render_views.verifyAuthToken, name="auth_token_sent"),
 ]
